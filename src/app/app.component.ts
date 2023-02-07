5bc976ae44a8afd17845fa3706de182e3cc821b0
Farah Azeem <farah.sarfaraz16@gmail.com>
2023-02-07 16:25:35 +1100

    Weather app using open weather api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
+import { WeatherData } from './models/weather.model';
+import {
+  faTemperatureEmpty,
+  faTemperatureFull,
+  faWind,
+  faDroplet,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'weather-around-the-world';
+export class AppComponent implements OnInit {
+  cityName: string = 'Sydney';
+  weatherData?: WeatherData;
+  faTemperatureEmpty = faTemperatureEmpty;
+  faTemperatureFull = faTemperatureFull;
+  faWind = faWind;
+  faDropletDegree = faDroplet;
+  constructor(private service: WeatherService) {}
+  ngOnInit() {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+
+  onSubmit() {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+
+  private getWeatherData(cityName: string) {
+    this.service.getWeatherData(cityName).subscribe({
+      next: (response) => {
+        this.weatherData = response;
+      },
+    });
+  }
 }
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '35981eec21mshb4038e42290abbap1f14c7jsnbd640816acb2'
+}
